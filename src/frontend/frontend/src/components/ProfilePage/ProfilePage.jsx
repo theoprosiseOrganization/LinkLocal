d960f4da0d25a873b58443e174e2a764b7e8bf10
Theo Prosise <97002616+theoprosise@users.noreply.github.com>
2025-06-26 14:18:52 -0700

    Created basic profile page framework

--- src/frontend/frontend/src/components/ProfilePage/ProfilePage.jsx
+++ src/frontend/frontend/src/components/ProfilePage/ProfilePage.jsx
@@ -1,12 +1,19 @@
 import Layout from "../Layout/Layout";
 import CreateEventButton from "../CreateEventPage/CreateEventButton";
+import VerticalEvents from "../VerticalEvents/VerticalEvents";
+import "./ProfilePage.css";
 
 export default function ProfilePage() {
-
   return (
     <Layout>
-      Profile Page
-      <CreateEventButton />
+      <div className="homepage-split">
+        <div className="homepage-left">
+          Your Events
+          <VerticalEvents />
+          <CreateEventButton />
+        </div>
+        <div className="homepage-right">Your Profile Information</div>
+      </div>
     </Layout>
   );
 }
