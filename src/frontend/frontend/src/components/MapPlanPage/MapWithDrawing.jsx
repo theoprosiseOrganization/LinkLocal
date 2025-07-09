4ba0e43655e9e15ac86fee3e57fff37589b520f9
Theo Prosise <97002616+theoprosise@users.noreply.github.com>
2025-07-09 14:56:17 -0700

    Using basic version

--- src/frontend/frontend/src/components/MapPlanPage/MapWithDrawing.jsx
+++ src/frontend/frontend/src/components/MapPlanPage/MapWithDrawing.jsx
@@ -1,98 +1,33 @@
-import React, { useRef, useEffect, useState, useCallback } from "react";
-import { APIProvider, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
-
+import React, { useEffect, useRef } from "react";
 const MAPS_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
-export default function MapWithDrawing({ events = [], currentLocation }) {
-  const mapRef = useRef(null);
-  const drawingManagerRef = useRef(null);
-  const [markers, setMarkers] = useState(events);
+export default function MapWithDrawing() {
+  const ref = useRef(null);
 
-  const onMapLoad = useCallback((map) => {
-    mapRef.current = map;
-
-    const dm = new google.maps.drawing.DrawingManager({
-      drawingMode: google.maps.drawing.OverlayType.POLYGON,
-      drawingControl: true,
-      drawingControlOptions: {
-        position: google.maps.ControlPosition.TOP_CENTER,
-        drawingModes: [google.maps.drawing.OverlayType.POLYGON],
-      },
-      polygonOptions: {
-        fillColor: "#2196F3",
-        fillOpacity: 0.2,
-        strokeWeight: 2,
-        clickable: false,
-        editable: false,
-        draggable: false,
-      },
-    });
-    dm.setMap(map);
-    drawingManagerRef.current = dm;
-
-    google.maps.event.addListener(dm, "polygoncomplete", handlePolygonComplete);
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${MAPS_KEY}&libraries=drawing`;
+    script.async = true;
+    script.onload = () => {
+      const map = new window.google.maps.Map(ref.current, {
+        center: { lat: 37.4845, lng: -122.1478 },
+        zoom: 10,
+      });
+      const drawingManager = new window.google.maps.drawing.DrawingManager({
+        drawingMode: null,
+        drawingControl: true,
+        drawingControlOptions: {
+          position: window.google.maps.ControlPosition.TOP_CENTER,
+          drawingModes: [
+            window.google.maps.drawing.OverlayType.POLYGON,
+            window.google.maps.drawing.OverlayType.CIRCLE,
+          ],
+        },
+      });
+      drawingManager.setMap(map);
+    };
+    document.body.appendChild(script);
   }, []);
 
-  const handlePolygonComplete = async (polygon) => {
-    const path = polygon
-      .getPath()
-      .getArray()
-      .map((latLng) => [latLng.lat(), latLng.lng()]);
-    if (
-      (path.length > 0 && path[0][0] !== path[path.length - 1][0]) ||
-      path[0][1] !== path[path.length - 1][1]
-    ) {
-      path.push(path[0]);
-    }
-
-    const geojson = {
-      type: "Polygon",
-      coordinates: [path],
-    };
-
-    // send to backend
-    // addPolygon(geojson)
-    // get events in polygon
-    polygon.setMap(null);
-  };
-
-  return (
-    <APIProvider apiKey={MAPS_KEY} libraries={["drawing"]}>
-      <div style={{ width: "100%", height: "100%" }}>
-        <Map
-          onLoad={onMapLoad}
-          defaultCenter={currentLocation || { lat: 37.4845, lng: -122.1478 }}
-          defaultZoom={10}
-          gestureHandling={"greedy"}
-          style={{ width: "80%", height: "80%" }}
-          options={{
-            disableDefaultUI: true,
-          }}
-        >
-          {/* Current location marker */}
-          {currentLocation && (
-            <AdvancedMarker
-              position={{
-                lat: currentLocation.lat,
-                lng: currentLocation.lng,
-              }}
-              title="Your Location"
-            />
-          )}
-
-          {/* Event markers */}
-          {markers.map((event) => (
-            <AdvancedMarker
-              key={event.id}
-              position={{
-                lat: ev.location.latitude,
-                lng: ev.location.longitude,
-              }}
-              title={ev.title}
-            />
-          ))}
-        </Map>
-      </div>
-    </APIProvider>
-  );
+  return <div ref={ref} style={{ width: "80vw", height: "80vh" }} />;
 }
