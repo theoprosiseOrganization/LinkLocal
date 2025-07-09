bdc3a4e4bc0e8695693785057fa8beda3bd03343
Theo Prosise <97002616+theoprosise@users.noreply.github.com>
2025-07-09 13:05:16 -0700

    add basic map with drawing

--- src/frontend/frontend/src/components/MapPlanPage/MapWithDrawing.jsx
+++ src/frontend/frontend/src/components/MapPlanPage/MapWithDrawing.jsx
@@ -12,7 +12,7 @@ export default function MapComponent({ events = [], currentLocation }) {
     mapRef.current = event.target;
 
     drawingManagerRef.current = new google.maps.drawing.DrawingManager({
-      drawingMode: google.maps.drawing.Overlaytyp.POLYGON,
+      drawingMode: google.maps.drawing.OverlayType.POLYGON,
       drawingControl: true,
       drawingControlOptions: {
         position: google.maps.ControlPosition.TOP_CENTER,
@@ -35,5 +35,74 @@ export default function MapComponent({ events = [], currentLocation }) {
       handlePolygonComplete
     );
   }, []);
-  
+
+  const handlePolygonComplete = async (polygon) => {
+    const path = polygon
+      .getPath()
+      .getArray()
+      .map((latLng) => [latLng.lat(), latLng.lng()]);
+    if (
+      (path.length > 0 && path[0][0] !== path[path.length - 1][0]) ||
+      path[0][1] !== path[path.length - 1][1]
+    ) {
+      path.push(path[0]);
+    }
+
+    const geojson = {
+      type: "Polygon",
+      coordinates: [path],
+    };
+
+    // send to backend
+    // addPolygon(geojson)
+    // get events in polygon
+    polygon.setMap(null);
+  };
+
+  return (
+    <APIProvider apiKey={MAPS_KEY} libraries={["drawing"]}>
+      <div style={{ width: "100%", height: "100%" }}>
+        <Map
+          defaultCenter={
+            currentLocation
+              ? { lat: currentLocation.lat, lng: currentLocation.lng }
+              : { lat: 37.4845, lng: -122.1478 }
+          }
+          defaultZoom={10}
+          gestureHandling={"greedy"}
+          style={{ width: "100%", height: "100%" }}
+          options={{
+            styles: [
+              {
+                disableDefaultUI: true,
+              },
+            ],
+          }}
+        >
+          {/* Current location marker */}
+          {currentLocation && (
+            <AdvancedMarker
+              position={{
+                lat: currentLocation.lat,
+                lng: currentLocation.lng,
+              }}
+              title="Your Location"
+            />
+          )}
+
+          {/* Event markers */}
+          {markers.map((event) => (
+            <AdvancedMarker
+              key={event.id}
+              position={{
+                lat: ev.location.latitude,
+                lng: ev.location.longitude,
+              }}
+              title={ev.title}
+            />
+          ))}
+        </Map>
+      </div>
+    </APIProvider>
+  );
 }
