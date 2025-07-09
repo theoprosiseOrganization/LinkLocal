78aea03919313e6e43c6938e19bc651461f38073
Theo Prosise <97002616+theoprosise@users.noreply.github.com>
2025-07-08 20:59:45 -0700

    Added database update functionality

---
+++ src/backend/services/recommendation_src/db_update.py
@@ -0,0 +1,18 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import pandas as pd
+
+load_dotenv()
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+def post_recommendations(user_id, recommendations):
+    data = {
+        "user_id": user_id,
+        "recommendations": recommendations
+    }
+    
+    supabase.table("Recommendations").upsert(data, on_conflict="user_id").execute()
