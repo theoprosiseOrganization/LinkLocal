e8ef40ec71675b220a6455eaa63689a97c21aaf4
Theo Prosise <97002616+theoprosise@users.noreply.github.com>
2025-07-14 13:02:49 -0700

    Add get top for user

--- src/backend/services/recommendation_src/recommender.py
+++ src/backend/services/recommendation_src/recommender.py
@@ -4,6 +4,7 @@ This module uses a scoring system to rank users based on their features and retu
 '''
 
 import heapq
+from feature_extraction import compute_features_for_user
 
 WEIGHTS = {
     "nearby": 0.5,
@@ -22,3 +23,11 @@ def get_top_k_recommendations(features, user_id, k=3):
         heapq.heappush(heap, (-score, other_user_id))
     top_k = [heapq.heappop(heap)[1] for _ in range(min(k, len(heap)))]
     return top_k
+
+def get_otp_k_recommendations_for_user(user_id, dist_map, candidates, weights, k):
+    df = compute_features_for_user(user_id, candidates, dist_map)
+    df['score'] = 0
+    for feature, weight in weights.items():
+        df['score'] += df[feature] * weight
+    top = df['score'].nlargest(k)
+    return [(uid, float(score)) for uid, score in top.items()]
