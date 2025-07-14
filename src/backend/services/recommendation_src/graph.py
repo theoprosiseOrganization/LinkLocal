8caf8ababb0968f597bce7159cbcd69f44c45f2e
Theo Prosise <97002616+theoprosise@users.noreply.github.com>
2025-07-14 12:43:55 -0700

    Add graph

---
+++ src/backend/services/recommendation_src/graph.py
@@ -0,0 +1,26 @@
+import networkx as nx
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+class FollowGraph:
+    def __init__(self):
+        self.graph = nx.DiGraph()
+        self.load_all()
+
+    def load_all(self):
+        resp = supabase.table("Follow").select("follower_id, followed_id").execute()
+        for {"follower_id": follower_id, "followed_id": followed_id} in resp.data:
+            self.graph.add_edge(follower_id, followed_id)
+
+    def add_edge(self, follower : str, following: str):
+        self.graph.add_edge(follower, following)
+
+    def bfs_distances(self, user_id: str, max_depth: int = 5):
+        distances = nx.single_source_shortest_path_length(self.graph, user_id, cutoff=max_depth)
+        return distances
